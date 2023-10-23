791feaae0b6f52d5b2d74a744a96e363ad5e03c7
Fey <manufety15@gmail.com>
2023-10-23 13:17:34 +0200

    Added HTML structure

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      // You can add more properties like notes or a checklist here.
+    }
+  }
+  
+  export { Todo };

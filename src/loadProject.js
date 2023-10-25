bae0691b38697227ec8af46562153fc9124c7e80
Fey <manufety15@gmail.com>
2023-10-25 14:14:56 +0200

    Rewrote the whole project

--- src/loadProject.js
+++ src/loadProject.js
@@ -1,63 +1,11 @@
-const createProjectForm = (content, action, existingValue) => {
-    const projectForm = document.createElement('form');
-    projectForm.id = 'project-form';
-  
-    const formTitle = document.createElement('h3');
-    formTitle.innerHTML = `${action} Project`;
-    projectForm.appendChild(formTitle);
-  
-    const projectTitleLabel = createFormLabel('Project Title', 'project-title');
-    projectForm.appendChild(projectTitleLabel);
-  
-    const projectTitleInput = createFormInput(
-      'project-title',
-      'text',
-      'Project Title',
-      'required',
-      existingValue
-    );
-    projectForm.appendChild(projectTitleInput);
-  
-    const submit = createFormButton(`${action} Project`, 'submit-project');
-    const cancel = createFormButton('Cancel', 'cancel-project', 'button');
-  
-    projectForm.appendChild(submit);
-    projectForm.appendChild(cancel);
-  
-    content.appendChild(projectForm);
-  };
-  
-  const createFormLabel = (text, htmlFor) => {
-    const label = document.createElement('label');
-    label.setAttribute('for', htmlFor);
-    label.innerHTML = text;
-    return label;
-  };
-  
-  const createFormInput = (name, type, placeholder, required, value) => {
-    const input = document.createElement('input');
-    input.setAttribute('name', name);
-    input.setAttribute('type', type);
-    input.setAttribute('placeholder', placeholder);
-    if (required) {
-      input.setAttribute('required', '');
-    }
-    if (value) {
-      input.setAttribute('value', value);
-    }
-    return input;
-  };
-  
-  const createFormButton = (text, id, type = 'submit') => {
-    const button = document.createElement('button');
-    button.setAttribute('type', type);
-    button.id = id;
-    button.innerHTML = text;
-    return button;
-  };
-  
-  const updateProjectName = (projectNameDOM, projectName) => {
-    projectNameDOM.innerHTML = projectName;
-  };
+const { v4: uuidv4 } = require('uuid');
 
-export { createProjectForm, updateProjectName };
+class ProjectModel {
+    constructor(title, todos = []) {
+        this.id = uuidv4();
+        this.title = title;
+        this.todos = todos;
+    }
+}
+
+export default ProjectModel;
--- src/loadTodo.js
+++ src/loadTodo.js
@@ -1,53 +1,14 @@
-import { format } from 'date-fns';
+const { v4: uuidv4 } = require('uuid');
 
-// Create and display a form for adding or editing a TODO
-function createTodoForm(content, action, existingTodo) {
-  const newForm = document.createElement('form');
-  newForm.id = 'todo-form';
-
-  // ... (Code for form creation)
-
-  content.appendChild(newForm);
+class ItemModel {
+    constructor(title, dueDate, description, priority, completionStatus=false) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.priority = priority;
+        this.completionStatus = completionStatus;
+        this.id = uuidv4();
+    }
 }
 
-// Render a TODO item on the page
-function renderTodo(toDosDOM, todoData) {
-  const { title, description, dueDate, priority, todoIndex, status } = todoData;
-
-  const todoDOM = document.createElement('section');
-  todoDOM.classList.add('todo');
-  todoDOM.dataset.index = todoIndex;
-
-  // ... (Code for rendering TODO item)
-
-  toDosDOM.appendChild(todoDOM);
-}
-
-// Toggle the visibility of TODO details
-function toggleTodoDetails(todoDOM) {
-  const expandButton = todoDOM.querySelector('.expand-todo');
-  expandButton.innerHTML = expandButton.innerHTML === '+' ? '-' : '+';
-
-  const detailsDOM = todoDOM.querySelector('.todo-details');
-  detailsDOM.classList.toggle('todo-details-hidden');
-}
-
-// Edit an existing TODO
-function editTodo(todoDOM, title, description, dueDate, priority) {
-  const titleDOM = todoDOM.querySelector('.todo-title');
-  titleDOM.innerHTML = title;
-
-  // ... (Code for updating other elements)
-
-  // Update the priority class
-  const priorityClasses = ['low-priority', 'medium-priority', 'high-priority'];
-  priorityClasses.forEach((cls) => todoDOM.classList.remove(cls));
-  todoDOM.classList.add(priority.toLowerCase() + '-priority');
-}
-
-// Toggle the completion status class
-function toggleTodoCompletionClass(todoDOM) {
-  todoDOM.classList.toggle('todo-completed');
-}
-
-export { createTodoForm, renderTodo, toggleTodoDetails, editTodo, toggleTodoCompletionClass };
+export default ItemModel
